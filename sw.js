0d826b0e65be0ab3a12ea2490b4cb4af04243872
Ethan Huerta <64386903+theinfomaniac@users.noreply.github.com>
2025-06-26 00:39:01 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,76 @@
+// Define the cache name
+const CACHE_NAME = 'student-tracker-v1';
+// List of files to cache
+const urlsToCache = [
+  '/',
+  '/index.html',
+  // You would add your CSS and other JS files here if they were separate
+  // 'styles.css',
+  // 'app.js',
+  'icon-192.png',
+  'icon-512.png'
+];
+
+// Install event: triggered when the service worker is first installed.
+self.addEventListener('install', event => {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch event: triggered for every request the page makes.
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response from the cache
+        if (response) {
+          return response;
+        }
+
+        // Not in cache - fetch from network
+        return fetch(event.request).then(
+          response => {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // IMPORTANT: Clone the response. A response is a stream
+            // and because we want the browser to consume the response
+            // as well as the cache consuming the response, we need
+            // to clone it so we have two streams.
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(cache => {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+    );
+});
+
+// Activate event: triggered when the service worker is activated.
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
