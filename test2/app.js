3a43efa00b2ef463cf59c7891445aba60984137c
Ethan Huerta <64386903+theinfomaniac@users.noreply.github.com>
2025-06-28 00:56:23 -0500

    Add files via upload

--- test2/app.js
+++ test2/app.js
@@ -1,3 +1,25 @@
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyCuC7sw59sw2g54d7EgnkpGr9I4UdOSUIk",
+    authDomain: "stutracker-af74a.firebaseapp.com",
+    projectId: "stutracker-af74a",
+    storageBucket: "stutracker-af74a.firebasestorage.app",
+    messagingSenderId: "379344021405",
+    appId: "1:379344021405:web:979140de8886ffdb1fb174",
+    measurementId: "G-2FSR03JET1"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
 document.addEventListener('DOMContentLoaded', () => {
     const sidebarContainer = document.getElementById('sidebar');
     if (!sidebarContainer) return;
